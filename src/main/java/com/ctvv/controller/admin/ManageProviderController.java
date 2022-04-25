ae86b0c9d006acf0b32f31590cda478640461e99
Truong Chu <quangchu12112002@gmail.com>
2022-04-25 23:04:56 +0700

    Hoan thanh liet ke danh sach va base them, sua, xoa

--- src/main/java/com/ctvv/controller/admin/ManageProviderController.java
+++ src/main/java/com/ctvv/controller/admin/ManageProviderController.java
@@ -1,36 +1,49 @@
 package com.ctvv.controller.admin;
 
+import com.ctvv.dao.ProductDAO;
+import com.ctvv.dao.ProviderDAO;
+import com.ctvv.model.Provider;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "ManageProviderController", value = "/admin/providers/*")
 public class ManageProviderController
 		extends HttpServlet {
-    final String HOME_PAGE = "/admin/manage/home.jsp";
-	final String SEARCH_SERVLET ="/admin/providers/search";
+	private final String HOME_PAGE = "/admin/manage/home.jsp";
+	private final String SEARCH_SERVLET = "/admin/providers/search";
+	private ProviderDAO providerDAO;
+
 	@Override
 	protected void doGet(
 			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String uri = request.getRequestURI();
-		if (uri.equals(request.getContextPath()+ SEARCH_SERVLET)){
-			search(request,response);
-		}
-		else
-			listProviders(request,response);
+		if (uri.equals(request.getContextPath() + SEARCH_SERVLET)) {
+			search(request, response);
+		} else
+			listProviders(request, response);
 	}
 
 	private void search(HttpServletRequest request, HttpServletResponse response) {
 
 	}
 
-	private void listProviders(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+	private void listProviders(
+			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Provider> providerList = providerDAO.getAll();
+		request.setAttribute("providerList", providerList);
 		goHome(request, response);
 	}
 
-	private void goHome(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private void goHome(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+	                                                                                     IOException {
 		request.setAttribute("tab", "providers");
 		RequestDispatcher dispatcher = request.getRequestDispatcher(HOME_PAGE);
 		dispatcher.forward(request, response);
@@ -40,9 +53,9 @@ public class ManageProviderController
 	protected void doPost(
 			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String action = request.getParameter("action");
-		switch (action){
+		switch (action) {
 			case "create":
-				create(request,response);
+				create(request, response);
 				break;
 			case "update":
 				update(request, response);
@@ -53,17 +66,57 @@ public class ManageProviderController
 		}
 	}
 
+	private void create(HttpServletRequest request, HttpServletResponse response) {
+		// Nhận các parameter có name là "name", "address", "phoneNumber", "email", "taxId"
+
+		// Phát hiện trùng tên, số điện thoại, email, mã số thuế
+		// (bằng cách sử dụng các hàm findByName, findByPhoneNumber, findByEmail, findByTaxId)
+
+
+		// set các attribute trong session: nameErrorMessage, phoneNumberMessage ... nếu phát hiện trùng lặp tên, sdt,
+		// sendRedirect về trang chính + /admin/providers
+
+		// goi ham create
+
+		// set attribute successMessage trong session nếu thành công
+		// sendRedirect về trang chính + /admin/providers
+	}
+
 	private void update(HttpServletRequest request, HttpServletResponse response) {
+		// Nhận các parameter có name là "id", "name", "address", "phoneNumber", "email", "taxId"
+
+		// Phát hiện trùng tên, số điện thoại, email, mã số thuế
+		// (bằng cách sử dụng các hàm findByName, findByPhoneNumber, findByEmail, findByTaxId)
+
+
+		// set các attribute trong session: nameErrorMessage, phoneNumberMessage ... nếu phát hiện trùng lặp tên, sdt,
+		// sendRedirect về trang chính + /admin/providers
+
+		// goi ham update
+
+		// set attribute successMessage trong session nếu thành công
+		// sendRedirect về trang chính + /admin/providers
+
+
 	}
 
 	private void delete(HttpServletRequest request, HttpServletResponse response) {
-	}
+		// Nhan parameter có name là "id"
+		// goi ham delete
+		// set successMessage trong session
+		// sendRedirect về trang chính + /admin/providers
 
-	private void create(HttpServletRequest request, HttpServletResponse response) {
 	}
 
 	@Override
 	public void init() throws ServletException {
 		super.init();
+		try {
+			Context context = new InitialContext();
+			DataSource dataSource = (DataSource) context.lookup("java:comp/env/jdbc/ctvv");
+			providerDAO = new ProviderDAO(dataSource);
+		} catch (NamingException e) {
+			throw new ServletException();
+		}
 	}
 }
--- src/main/java/com/ctvv/model/Provider.java
+++ src/main/java/com/ctvv/model/Provider.java
@@ -1,4 +1,97 @@
 package com.ctvv.model;
 
+import java.util.Objects;
+
 public class Provider {
+	int providerId;
+	String providerName;
+	String address;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Provider provider = (Provider) o;
+		return providerId == provider.providerId || providerName.equals(provider.providerName)  || phoneNumber.equals(provider.phoneNumber) || email.equals(provider.email) && taxId.equals(provider.taxId);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(providerId, providerName, address, phoneNumber, email, taxId);
+	}
+
+	String phoneNumber;
+	String email;
+	String taxId;
+
+	public Provider(
+			int providerId, String providerName, String address, String phoneNumber, String email,
+			String taxId) {
+		this.providerId = providerId;
+		this.providerName = providerName;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.taxId = taxId;
+	}
+
+	public Provider(String providerName, String address, String phoneNumber, String email, String taxId) {
+		this.providerName = providerName;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.taxId = taxId;
+	}
+
+	public Provider() {
+
+	}
+
+	public int getProviderId() {
+		return providerId;
+	}
+
+	public void setProviderId(int providerId) {
+		this.providerId = providerId;
+	}
+
+	public String getProviderName() {
+		return providerName;
+	}
+
+	public void setProviderName(String providerName) {
+		this.providerName = providerName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTaxId() {
+		return taxId;
+	}
+
+	public void setTaxId(String taxId) {
+		this.taxId = taxId;
+	}
 }
