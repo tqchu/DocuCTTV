9040977336ee78d04f7695451d822286f9b3a638
dochautrinh <trinhdc197@gmail.com>
2022-05-04 22:53:43 +0700

    nothing change

--- src/main/java/com/ctvv/controller/admin/ManageInventoryController.java
+++ src/main/java/com/ctvv/controller/admin/ManageInventoryController.java
@@ -99,7 +99,6 @@ public class ManageInventoryController
 	@Override
 	protected void doPost(
 			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 	}
 
 	@Override
